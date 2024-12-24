342599c39d402e3cb889ebfc924e9920018bc07d
Lounis <lounisabdellou.dev@gmail.com>
2024-12-24 17:28:14 +0100

    ok

--- RUST_01/ex06/src/lib.rs
+++ RUST_01/ex06/src/lib.rs
@@ -1,4 +1,29 @@
-pub fn big_add(a: &[u8], b: &[u8]) -> Vec<u8> {}
+fn convert_to_vector(bytes: &[u8]) -> Vec<u8> {
+    let mut i = 0;
+    let mut j = 0;
+    let mut vector: Vec<u8> = Vec::new();
+
+    while i < bytes.len() && bytes[i] - 48 == 0 {
+        i += 1;
+    }
+
+    while i + j < bytes.len() {
+        if !bytes[i + j].is_ascii_digit() {
+            panic!("params must only contain ascii digits");
+        }
+
+        vector.push(bytes[i + j]);
+        j += 1;
+    }
+
+    return vector;
+}
+
+pub fn big_add(a: &[u8], b: &[u8]) -> Vec<u8> {
+    let mut res: Vec<u8> = Vec::new();
+
+    return res;
+}
 
 #[cfg(test)]
 #[test]
